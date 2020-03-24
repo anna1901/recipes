870c83f4bc0a0c31f8283289b734d66131502f7d
Ania Olak <anna.olak@lunarlogic.io>
2020-03-24 20:33:39 +0100

    Try to fix bootstrap on heroku

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
 
-import '../stylesheets/application.scss'
+// import '../stylesheets/application.scss'
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
