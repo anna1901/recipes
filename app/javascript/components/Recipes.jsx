0b91608f2b96ad50c6c897511d44231c14942ea4
Ania Olak <anna.olak@lunarlogic.io>
2020-03-26 10:18:43 +0100

    Add rails views and remove react router

--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -33,7 +33,7 @@ class Recipes extends React.Component {
           />
           <div className="card-body">
             <h5 className="card-title">{recipe.name}</h5>
-            <a href={`/recipe/${recipe.id}`} className="btn custom-button">View Recipe</a>
+            <a href={`/recipes/${recipe.id}`} className="btn custom-button">View Recipe</a>
           </div>
         </div>
       </div>
--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -3,12 +3,26 @@ import { render } from "react-dom";
 import $ from 'jquery';
 import Popper from 'popper.js';
 import Homepage from "../components/Homepage";
+import Recipe from "../components/Recipe";
 
 // import '../stylesheets/application.scss'
 
 document.addEventListener("DOMContentLoaded", () => {
-  render(
-    <Homepage />,
-    document.body.appendChild(document.createElement("div"))
-  );
+  const homepage = document.getElementById('homepage');
+  if (homepage) {
+    render(
+      <Homepage />,
+      homepage
+    );
+  }
+
+  const recipeShow = document.getElementById('recipe-show');
+  if (recipeShow) {
+    const node = document.getElementById('recipe_id')
+    const data = JSON.parse(node.getAttribute('data'))
+    render(
+      <Recipe recipe_id={data} />,
+      recipeShow
+    );
+  }
 });
