534f785956a909d0249f026f50b4178debac3182
Ania Olak <anna.olak@lunarlogic.io>
2020-03-26 20:54:21 +0100

    Change the button on homepage depending if user is signed in

--- app/javascript/components/HomepageHeader.jsx
+++ app/javascript/components/HomepageHeader.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-export default () => (
-  <div className="vw-100 vh-40 primary-color d-flex align-items-center justify-content-center">
+const HomepageHeader = (props) => {
+  const renderButton = (signed_in) => {
+    if(signed_in) {
+      return <a href="/recipes" className="btn btn-lg custom-button">View all Recipes</a>
+    } else {
+      return <a href="/users/sign_up" className="btn btn-lg custom-button">Sign Up</a>
+    };
+  }
+  return(
+    <div className="vw-100 vh-40 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
         <h1 className="display-4">Food Recipes</h1>
@@ -10,8 +17,11 @@ export default () => (
           A carefully selected list of recipes for the best homemade meal and delicacies.
         </p>
         <hr className="my-4" />
-        <a href="/recipes" className="btn btn-lg custom-button">View all Recipes</a>
+        { renderButton(props.signed_in) }
       </div>
     </div>
   </div>
-);
+  );
+}
+
+export default HomepageHeader
