08d0c60d8e70589e0e8d1fc9740c30ef49f64801
Ania Olak <anna.olak@lunarlogic.io>
2020-03-27 10:26:56 +0100

    Change homepage header h1 size and navbar color

--- app/javascript/components/HomepageHeader.jsx
+++ app/javascript/components/HomepageHeader.jsx
@@ -16,18 +16,18 @@ const HomepageHeader = (props) => {
     };
   }
   return(
-    <div className="vw-100 vh-40 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Food Recipes</h1>
-        <p className="lead">
-          A carefully selected list of recipes for the best homemade meal and delicacies.
-        </p>
-        <hr className="my-4" />
-        { renderButton(props.signed_in) }
+    <div className="vw-100 vh-75 primary-color d-flex align-items-center justify-content-center">
+      <div className="jumbotron jumbotron-fluid bg-transparent">
+        <div className="container secondary-color">
+          <h1 className="display-2">Food Recipes</h1>
+          <p className="lead">
+            A carefully selected list of recipes for the best homemade meal and delicacies.
+          </p>
+          <hr className="my-4" />
+          { renderButton(props.signed_in) }
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
