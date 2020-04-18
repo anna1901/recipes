7367eb1f8a74475d765f0e0831bb64fad3b335ee
Ania Olak <anna.olak@lunarlogic.io>
2020-04-18 22:16:11 +0200

    Add feature spec for user login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -6,42 +6,30 @@ feature 'Login page: Login with invalid email or password' do
   end
 
   scenario 'Show invalid email or password error to user' do
-    error_message = 'Błędny adres email lub hasło.'
-    fill_in('E-mail', with: 'wrong_email@example.com')
-    fill_in('Hasło', with: 'wrong_password')
-    click_button('Zaloguj')
+    error_message = 'Invalid Email or password.'
+    fill_in('Email', with: 'wrong_email@example.com')
+    fill_in('Password', with: 'wrong_password')
+    click_button('Log in')
     expect(page).to have_content(error_message)
   end
 end
 
 feature 'Login page: valid credentials' do
   background do
-    visit admin_root_path
+    visit new_user_session_path
   end
 
   context 'Log in as user' do
     let!(:user) { FactoryBot.create(:user) }
 
-    scenario 'Show no admin permissions error and redirect to home page' do
-      error_message = 'Nie masz uprawnień administratora'
-      fill_in('E-mail', with: user.email)
-      fill_in('Hasło', with: user.password)
-      click_button('Zaloguj')
-      expect(page).to have_content(error_message)
-      expect(page).to have_current_path(new_user_session_path)
+    scenario 'Show successful login alert and redirect to home page' do
+      alert = 'Signed in successfully.'
+      fill_in('Email', with: user.email)
+      fill_in('Password', with: user.password)
+      click_button('Log in')
+      expect(page).to have_content(alert)
+      expect(page).to have_current_path(root_path)
     end
   end
 
-  context 'Log in as admin' do
-    let!(:admin) { FactoryBot.create(:admin) }
-
-    scenario 'Show welcome message in admin panel' do
-      welcome_message = 'Witaj w panelu Administratora'
-      fill_in('E-mail', with: admin.email)
-      fill_in('Hasło', with: admin.password)
-      click_button('Zaloguj')
-      expect(page).to have_content(welcome_message)
-      expect(page).to have_current_path(admin_root_path)
-    end
-  end
 end
