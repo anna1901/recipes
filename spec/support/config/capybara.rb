f8d3b0aaeddc68a58a0fbb3bb410d86ab71230d5
Ania Olak <anna.olak@lunarlogic.io>
2020-04-18 23:16:56 +0200

    Add configuration for capybara

---
+++ spec/support/config/capybara.rb
@@ -0,0 +1,15 @@
+require 'capybara/rails'
+require 'capybara/rspec'
+
+Capybara.default_driver = :selenium_chrome_headless
+Capybara.default_max_wait_time = 2
+
+RSpec.configure do |config|
+  config.before(:each) do |example|
+    Capybara.current_driver = :selenium_chrome if example.metadata[:chrome] || ENV['CHROME']
+  end
+
+  config.after(:each) do
+    Capybara.use_default_driver
+  end
+end
